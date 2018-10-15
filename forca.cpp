f6dd9f562a9d68cdaa719a91e26aad3932511604
gafeol <gabfeol@gmail.com>
2018-10-15 10:57:14 -0300

    Aula 6.3

--- forca.cpp
+++ forca.cpp
@@ -2,9 +2,19 @@
 #include <string>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA"; 
+
+bool letra_existe(char chute){
+    for(int i = 0; i < PALAVRA_SECRETA.size();i++){
+        if(chute == PALAVRA_SECRETA[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main () {
-    string palavra_secreta = "MELANCIA"; 
-    cout << palavra_secreta << endl;
+    cout << PALAVRA_SECRETA << endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
@@ -13,6 +23,11 @@ int main () {
         char chute;
         cin >> chute;
 
-        cout << "O seu chute foi " << chute << endl;
+        if(letra_existe(chute)){
+            cout << "Você acertou! Seu chute está na palavra." << endl;
+        }
+        else{
+            cout << "Você errou! Seu chute não está na palavra." << endl;
+        }
     }
 }
