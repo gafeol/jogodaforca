84bdfc085c9e054b76d58cf5f2e7f2637b50e02e
gafeol <gabfeol@gmail.com>
2018-10-15 12:31:56 -0300

    Aula 7.1

--- forca.cpp
+++ forca.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA"; 
+map<char, bool> chutou;
 
 bool letra_existe(char chute){
     for(char letra : PALAVRA_SECRETA){
@@ -13,15 +15,26 @@ bool letra_existe(char chute){
     return false;
 }
 
-int main () {
-    cout << PALAVRA_SECRETA << endl;
 
+int main () {
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
     while(nao_acertou && nao_enforcou){
+        for(char letra : PALAVRA_SECRETA){
+            if(chutou[letra]){
+                cout << letra << " ";
+            }
+            else{
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
         char chute;
         cin >> chute;
+
+        chutou[chute] = true;
 
         if(letra_existe(chute)){
             cout << "Você acertou! Seu chute está na palavra." << endl;
